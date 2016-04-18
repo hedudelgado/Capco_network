d7c71593fde400864efd57ebbc3f199b0ae17995
hedudelgado <hedu.delgado@gmail.com>
2016-04-18 02:10:03 +0100

    user has a timeline, test passing

--- src/main/java/com/capco/capco_network/domain/Timeline.java
+++ src/main/java/com/capco/capco_network/domain/Timeline.java
@@ -1,9 +1,11 @@
 package com.capco.capco_network.domain;
 
+import java.util.ArrayList;
+
 /**
  * Created by sorry on 17/04/2016.
  */
 public class Timeline {
-    public String[] messages;
+    private ArrayList<String>messages = new ArrayList<String>();
 
 }
